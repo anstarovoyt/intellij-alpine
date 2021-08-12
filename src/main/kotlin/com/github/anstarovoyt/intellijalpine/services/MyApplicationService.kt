374cc100af57d9169d291e4de189e8b0d9ecf604
GitHub Action <action@github.com>
2021-08-12 19:05:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/anstarovoyt/intellijalpine/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.anstarovoyt.intellijalpine.services
+
+import com.github.anstarovoyt.intellijalpine.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/anstarovoyt/intellijalpine/services/MyProjectService.kt
+++ src/main/kotlin/com/github/anstarovoyt/intellijalpine/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.anstarovoyt.intellijalpine.services
 
+import com.github.anstarovoyt.intellijalpine.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
